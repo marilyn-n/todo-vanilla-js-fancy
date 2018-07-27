8718695697995d5476f5e63a4f5dcfa8f3ef59f7
Ravi Kalia <ravkalia@gmail.com>
2018-07-27 14:49:49 -0500

    remove functionality now working

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
 // icons code
-const removeSVG = ` <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+const removeSVG = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22" /><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3
    c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9
    C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7
@@ -8,18 +8,9 @@ viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve
      /></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8
      C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>`;
 
- const completeSVG = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+const completeSVG = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
  viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8
   c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>`;
-
-
-document.getElementById('add').addEventListener('click', () => {
-  const {value} = document.getElementById('item');
-  if (value) {
-    addItemTodo(value);
-    document.getElementById('item').value = '';
-  }
-})
 
 const addItemTodo = (text) => {
   const list = document.getElementById('todo');
@@ -38,11 +29,19 @@ const addItemTodo = (text) => {
   buttons.appendChild(remove);
   buttons.appendChild(complete);
   item.appendChild(buttons);
-  list.insertBefore(item, list.childNodes[0]);  
-}; 
+  list.insertBefore(item, list.childNodes[0]);
+};
 
-const removeItem = () => {  
+const removeItem = function() {
   const item = this.parentNode.parentNode;
   const parent = item.parentNode;
   parent.removeChild(item);
 };
+
+document.getElementById('add').addEventListener('click', () => {
+  const { value } = document.getElementById('item');
+  if (value) {
+    addItemTodo(value);
+    document.getElementById('item').value = '';
+  }
+});
