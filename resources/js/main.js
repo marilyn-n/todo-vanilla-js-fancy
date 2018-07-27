9f047f2d8c07d698334d8acdcfbc5de5018f8fbd
Ravi Kalia <ravkalia@gmail.com>
2018-07-27 12:45:14 -0500

    adding new item to todo list via dom tree manipulations

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,28 @@
+// icons code
+const removeSVG = ``;
+const completeSVG = ``;
+
+
 document.getElementById('add').addEventListener('click', () => {
   const value = document.getElementById('item').value;
   if(value) addItemTodo(value);
 })
 
-const addItemTodo = (value) => {
-  console.log(value);
-};
+const addItemTodo = (text) => {
+  const list = document.getElementById('todo');
+  const item = document.createElement('li');
+  item.innerText = text;
+  const buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+  const remove  = document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML = removeSVG;
+  const complete  = document.createElement('button');
+  complete.classList.add('complete');
+  complete.innerHTML = completeSVG;
+
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+  list.appendChild(item);  
+}; 
